86e1e6401e27db5721e9ef1225de625c356f26b2
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-08-25 21:15:17 +0000

    chore: add commitlint [#1]

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/**
+ * @description Configuration for commitlint
+ * @author      Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
+ * {@link       https://commitlint.js.org/}   
+ * {@link       https://www.conventionalcommits.org/en/v1.0.0/}
+ */
+module.exports = {
+    /**
+     * @note Resolve and load `@commitlint/config-conventional` 
+     *       from `node_modules`
+     */
+    extends: ['@commitlint/config-conventional']
+};
