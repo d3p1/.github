a8eb128fb154a4fe94a1c57a6c14e6b34517fd7f
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-08-26 14:01:43 +0000

    chore: add a parser preset to commitlint [#1]

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,5 +9,10 @@ module.exports = {
      * @note Resolve and load `@commitlint/config-conventional` 
      *       from `node_modules`
      */
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+
+    /**
+     * @note Add parser preset
+     */
+    parserPreset: './commitlint.parser.js'
 };
